0190e71a17519c2052658e294dfd58e590494d7d
ramperto devops <rampertodevops@Memberids-MacBook-Pro-3.local>
2019-06-28 15:26:56 +0700

    add mysql db dump

---
+++ dump_exclude.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+PASSWORD=PASSWORD
+HOST=host
+USER=user
+DATABASE=member_test
+DB_FILE=dump_member_test.sql
+EXCLUDED_TABLES=(
+table1
+table2
+table3
+)
+
+IGNORED_TABLES_STRING=''
+for TABLE in "${EXCLUDED_TABLES[@]}"
+do :
+   IGNORED_TABLES_STRING+=" --ignore-table=${DATABASE}.${TABLE}"
+done
+
+echo "Dump structure"
+mysqldump --host=${HOST} --user=${USER} --password=${PASSWORD} --single-transaction --no-data --routines ${DATABASE} > ${DB_FILE}
+
+echo "Dump content"
+mysqldump --host=${HOST} --user=${USER} --password=${PASSWORD} ${DATABASE} --no-create-info --skip-triggers ${IGNORED_TABLES_STRING} >> ${DB_FILE}

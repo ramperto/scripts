3bc50b3a7d6a028616ac782112bb8af349ec09a5
ramperto devops <rampertodevops@Memberids-MacBook-Pro-3.local>
2019-07-11 15:17:32 +0700

    add personal key

--- mysql_dump_daily.sh
+++ mysql_dump_daily.sh
@@ -1,8 +1,12 @@
 DBNAME=focus
 DATE=`date +"%Y%m%d"`
 SQLFILE=$DBNAME-${DATE}.sql
+
+# usual dump command
 mysqldump $DBNAME | gzip -c > /home/perto/backup/$SQLFILE.gz
 
+#dumping GTID master db
+mysqldump $DBNAME --flush-privileges --single-transaction --set-gtid-purged=off --flush-logs --triggers | gzip -c > $SQLFILE.gz
 
 #delete after >n days
 
